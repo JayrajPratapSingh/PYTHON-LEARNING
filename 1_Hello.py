63862325b5ef807cfa7592a00f8173b0be455d70
Jayraj <jairaj273310@gmail.com>
2023-10-01 10:42:15 +0530

    updating first hello world program and indentaion

---
+++ 1_Hello.py
@@ -0,0 +1,5 @@
+print("hello world, \nThis is Jayraj");
+
+# this should print hello world\
+
+print("hello machine");
---
+++ 2_pythonIndentation.py
@@ -0,0 +1,9 @@
+# Python Indentation
+#jab pyhon programming ki jati hai to code same place se next line me start hona chahiye..
+ #   print("print statement") wrong indentation
+# Other programming language me ham bracket ka istemal karte ek block of code likhne k liye hai but python me ham identation ka istemal karte hai
+# In other programming language we use brackets for block of codes but in python we use indentaion for block of codes
+#Right indentation given below
+print("statement1")
+print("statement2")
+
